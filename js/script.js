87124c06d1991764f20817c45abd259e1cc36502
Lucy Stickney <lustickney@gmail.com>
2024-07-19 15:24:28 -0500

    fixing button functionality and adding photos

--- js/script.js
+++ js/script.js
@@ -73,3 +73,74 @@ function showPaintings() {
     })
 }
 
+function togglePhoto() {
+    photos.forEach(element => {
+        element.classList.toggle('hideElems');
+    })
+    photoButton.forEach(element => {
+        element.classList.toggle('boldTitle');
+    })
+}
+
+function togglePaint() {
+    paintings.forEach(element => {
+        element.classList.toggle('hideElems');
+    })
+    paintingButton.forEach(element => {
+        element.classList.toggle('boldTitle');
+    })
+}
+
+function toggleDrawing() {
+    drawings.forEach(element => {
+        element.classList.toggle('hideElems');
+    })
+    drawingButton.forEach(element => {
+        element.classList.toggle('boldTitle');
+    })
+}
+
+function photoClick() {
+    sessionStorage.setItem('currentArt', 'photo');
+    console.log("set to photo");
+}
+
+function drawClick() {
+    sessionStorage.setItem('currentArt', 'drawing');
+    console.log("set to drawing");
+}
+
+function paintClick() {
+    sessionStorage.setItem('currentArt', 'painting');
+    console.log("set to paint");
+}
+function reset() {
+    sessionStorage.setItem('currentArt', 'all');
+    console.log("set to all");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // when works page is loaded
+    // check what currentArt is
+    let curr = sessionStorage.getItem('currentArt');
+    console.log(curr);
+    if (curr) { 
+        // show the art of currentArt
+        switch (curr) {
+            case 'photo':
+                togglePhoto();
+                break;
+            case 'drawing':
+                toggleDrawing();
+                break;
+            case 'painting':
+                togglePaint();
+                break;
+            case 'all':
+                // if no currentArt, show all
+                togglePaint();
+                toggleDrawing();
+                togglePhoto();
+        }
+    }  
+})
